5489605b44a5d605bcb93f32b0345922c6885492
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-19 19:05:39 +0530

    comop

--- src/PreviousNextMethods.js
+++ src/PreviousNextMethods.js
@@ -9,7 +9,6 @@ import { CgNotes } from "react-icons/cg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { SiAdobeacrobatreader } from "react-icons/si";
-import Cards from "./Cards";
 
 export default class PreviousNextMethods extends Component {
   constructor(props) {
