da760caae12c19d6a0f2bb4e2f41369423edbbf7
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-19 17:51:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 import React from "react";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import { AiOutlineYoutube } from "react-icons/ai";
-import { BiTimeFive } from "react-icons/bi";
-import { GoPrimitiveDot } from "react-icons/go";
-import Main from "./Main";
+
 import PreviousNextMethods from "./PreviousNextMethods";
 
 function App() {
--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,6 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { BiTimeFive } from "react-icons/bi";
 import { GoPrimitiveDot } from "react-icons/go";
-import Slider from "react-slick";
 
 const Main = () => {
   const settings = {
